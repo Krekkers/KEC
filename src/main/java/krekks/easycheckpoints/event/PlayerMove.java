43a4a1a2281bfc0eff8d1ec83b47005ce5256c38
Krekkers <krekkertime@gmail.com>
2022-08-01 22:36:44 +0200

    v1.4.1 WIP

--- src/main/java/krekks/easycheckpoints/event/PlayerMove.java
+++ src/main/java/krekks/easycheckpoints/event/PlayerMove.java
@@ -7,10 +7,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 import static krekks.easycheckpoints.EasyCheckpoints.*;
-import static krekks.easycheckpoints.misc.PlayerBoost.Boost;
+import static krekks.easycheckpoints.misc.PlayerBoost.boost;
+import static krekks.easycheckpoints.misc.PlayerBoost.elytraBoost;
 import static krekks.easycheckpoints.playerdata.PlayerDataHandler.*;
 
 public class PlayerMove implements Listener {
@@ -18,17 +20,20 @@ public class PlayerMove implements Listener {
     //tip from thiemo to not check in on move!
     //materials
     Material checkpoint = Material.matchMaterial(config.getString("checkpointblock"));
-    Material boost = Material.matchMaterial(config.getString("jumpblock"));
-    Material speed = Material.matchMaterial(config.getString("boostblock"));
+    Material jump = Material.matchMaterial(config.getString("jumpblock"));
+    Material boost = Material.matchMaterial(config.getString("boostblock"));
+    Material elytra = Material.matchMaterial(config.getString("elytrablock"));
     Material finish = Material.matchMaterial(config.getString("finishblock"));
     //text
     String jumpText = config.getString("jumpmessage");
     String checkpointText = config.getString("checkpointmessage");
     String boostText = config.getString("boostmessage");
+    String elytraText = config.getString("elytramessage");
     //sounds
     Sound jumpSound = Sound.BLOCK_PISTON_EXTEND;
     Sound checkpointSound = Sound.BLOCK_NOTE_BLOCK_PLING;
     Sound boostSound = Sound.ENTITY_ENDER_DRAGON_FLAP;
+    Sound elytraSound = Sound.ENTITY_EGG_THROW;
     /* Removed due to 1.19 support
     Sound jumpSound = Sound.valueOf(config.getString("jumpsound"));
     Sound checkpointSound = Sound.valueOf(config.getString("checkpointsound"));
@@ -37,6 +42,7 @@ public class PlayerMove implements Listener {
     //values
     double jumpVal = config.getDouble("jumpvalue");
     double boostVal = config.getDouble("boostvalue");
+    double elytraVal = config.getDouble("elytravalue");
     //
     boolean checkpointOnly = config.getBoolean("checkpointonly");
     @EventHandler
@@ -51,7 +57,6 @@ public class PlayerMove implements Listener {
             setCheckpointOf(p, loc);
             p.sendMessage(ChatColor.translateAlternateColorCodes('&',checkpointText));
             p.playSound(p.getLocation(), checkpointSound,1,2);
-
         }
         //this is the part for the finishline
         if(p.getLocation().add(0,-1,0).getBlock().getType() == finish && !e.getPlayer().hasPermission("krekks.perms")){
@@ -71,19 +76,24 @@ public class PlayerMove implements Listener {
         }
         if(checkpointOnly) return;
         //jump boost
-        if(p.getLocation().add(0,-1,0).getBlock().getType() == boost){
-            Boost(new Vector(0,jumpVal / 10,0), e.getPlayer(),jumpSound, jumpText);
+        if(p.getLocation().add(0,-1,0).getBlock().getType() == jump){
+            boost(new Vector(0,jumpVal / 10,0), e.getPlayer(),jumpSound, jumpText);
+        }
+        //elytra boost
+        else if(p.getLocation().add(0,-1,0).getBlock().getType() == elytra){
+            elytraBoost(e.getPlayer(),elytraVal,elytraSound , elytraText);
         }
         //forward boost
-        if(p.getLocation().add(0,-1,0).getBlock().getType() == speed){
-            Boost(new Vector(e.getPlayer().getLocation().getDirection().getX(),boostVal / 10,e.getPlayer().getLocation().getDirection().getZ()), e.getPlayer(),boostSound , boostText);
+        else if(p.getLocation().add(0,-1,0).getBlock().getType() == boost){
+            boost(new Vector(e.getPlayer().getLocation().getDirection().getX(),boostVal / 10,e.getPlayer().getLocation().getDirection().getZ()), e.getPlayer(),boostSound , boostText);
         }
-
+        if(!p.isGliding() &&  p.getInventory().getChestplate().getType() == Material.ELYTRA){
+            p.getInventory().setChestplate(new ItemStack(Material.AIR));
+        }
     }
 
     @EventHandler
     void FallDamage(EntityDamageEvent e){
-        if(!Toggle) return;
         if (e.getCause() == EntityDamageEvent.DamageCause.FALL && Toggle) e.setCancelled(true);
     }
 }
